fc3658b588a46f339635cb6fe8edd4b771a246f0
BlackBold <toantran014@gmail.com>
2022-06-03 21:08:32 +0700

    Update final file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // I pratice project from Chris Coyier
 
 // theme color from localStorage
-document.documentElement.style.setProperty("--primary-color", localStorage.getItem("userThemeColor"));
+document.documentElement.style.setProperty("--clr-primary", localStorage.getItem("userThemeColor"));
 
-var colorInput = document.querySelector("#choose-theme-color");
+const colorInput = document.querySelector("#choose-theme-color");
 
 colorInput.addEventListener("change", function() {
-  document.documentElement.style.setProperty("--primary-color", this.value);
-  localStorage.setItem("UserThemeColor", this.value);
+  document.documentElement.style.setProperty("--clr-primary", this.value);
+  localStorage.setItem("userThemeColor", this.value);
 });
